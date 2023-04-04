e35a1308fcac6533f9b39b00c6381a6b919c3b49
Mikros1999 <maksimovic.miroslav999@gmail.com>
2023-04-04 11:59:52 +0200

    adding step and locking of pieces
    adding step so pieces move on their own, as well as locking piece in place when it reaches bottom or other piece

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -12,12 +12,20 @@ public class Piece : MonoBehaviour
     public Vector3Int position { get; private set; }
     public int rotationIndex { get; private set; }
 
+    public float stepDelay = 1f;
+    public float lockDelay = 0.5f;
+
+    private float stepTime;
+    private float lockTime;
+
     public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
         this.board = board;
         this.position = position;
         this.data = data;
         this.rotationIndex = 0;
+        this.stepTime = Time.time + this.stepDelay;
+        this.lockTime = 0f;
 
         if (this.cells == null)
         {
@@ -33,6 +41,8 @@ public class Piece : MonoBehaviour
     private void Update()
     {
         this.board.Clear(this);
+
+        this.lockTime += Time.deltaTime;
 
         if (Input.GetKeyDown(KeyCode.Q)) // rotate
         {
@@ -63,9 +73,34 @@ public class Piece : MonoBehaviour
             {
                 continue;
             }
+            
+            Lock();
+        }
+
+        if (Time.time >= this.stepTime)
+        {
+            Step();
         }
 
         this.board.Set(this);
+    }
+
+    private void Step()
+    {
+        this.stepTime = Time.time + this.stepDelay;
+
+        Move(Vector2Int.down);
+
+        if (this.lockTime >= this.lockDelay)
+        {
+            Lock();
+        }
+    }
+
+    private void Lock()
+    {
+        this.board.Set(this);
+        this.board.SpawnPiece();
     }
 
     private bool Move(Vector2Int translation)
@@ -79,6 +114,7 @@ public class Piece : MonoBehaviour
         if (valid)
         {
             this.position = newPosition;
+            this.lockTime = 0f;
         }
 
         return valid;
